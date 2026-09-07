58b60a20f5358ef8b0b28c7880ff5f8dc52ecc14
Nyuykongmo alvin <nyuykongmoalvin848@gmail.com>
2026-09-07 16:14:23 +0100

    feat: fix blank page deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-  // base: '/EXPENSE-TRACKER1/'
+  plugins: [react()],
+  base: '/EXPENSE-TRACKER1/'
 })
